c9e238a83368fdf69402c732bee6d6fe2e164f5c
shre111 <dantanishreya@gmail.com>
2025-02-22 22:31:44 +0530

    feate: added button with state change value

--- App.tsx
+++ App.tsx
@@ -9,19 +9,15 @@ import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  Button,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
@@ -61,25 +57,19 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  const pressButton =()=>{
+    console.warn("Called")
+  }
 
   return (
     <View style={backgroundStyle}>
       <ScrollView
         style={backgroundStyle}>
           <Section title="Here we go">
-            Helloooowww <Text style={styles.highlight}>App.tsx</Text> to change this
+            Helloooowww  <Text style={styles.highlight}>Shreya dantani</Text> to change this
             screen and then come back to see your edits.
           </Section>
+          <Button title="press Here" onPress={pressButton}/>
       </ScrollView>
     </View>
   );
